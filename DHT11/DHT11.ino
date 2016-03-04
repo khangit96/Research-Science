73aae50c96a28bf41cdac8404ef14f6ba3b42aa1
khangit96 <khangit69@gmail.com>
2016-03-04 20:16:00 +0700

    fdfsksdk

---
+++ DHT11/DHT11.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+ 
+  dht.begin();
+}
+
+void loop() {
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+}
