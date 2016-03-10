5506dbaf365360a8d2c0486924d3c9e2b9bbd57f
khangit96 <khangit69@gmail.com>
2016-03-10 09:43:15 +0700

    sdksfkfs

--- LED_bluetooth/LED_bluetooth.ino
+++ LED_bluetooth/LED_bluetooth.ino
@@ -1,68 +1,36 @@
+int count=0;
+String test;
 char command;
-String string;
-boolean ledon = false;
 #define led 4
-#define led2 8
- void setup()
- {
-     Serial.begin(9600);
-     pinMode(led, OUTPUT);
-     pinMode(led2, OUTPUT);
- }
- void loop()
- {
-     if (Serial.available() > 0)
-     {
-       string = "";
-     
-     }
-     while(Serial.available() > 0)
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{ 
+    while(Serial.available() > 0)
      {
        command = ((byte)Serial.read());
-       if(command == ':')
-       {
-       break;
-       }
-       else
-       {
-       string += command;
-       }
+       test += command;
+       
        delay(1);
      }
-     if(string == "TURN ON")
-      {
-       ledOn();
-        digitalWrite(led2,HIGH);
-       ledon = true;
-       Serial.println(string);
-     }
-     if(string =="TURN OFF")
+     if(test=="TURN ON")
      {
-       ledOff();
-       digitalWrite(led2,LOW);
-       ledon = false;
-       Serial.println(string); //debug
+       digitalWrite(led,HIGH);
+       Serial.print("S");
+       test="";
+      command='\0';
      }
-    /* if ((string.toInt()>=0)&&(string.toInt()<=255))
+     if(test=="TURN OFF")
      {
-       if (ledon==true)
-       {
-       analogWrite(led, string.toInt());
-       Serial.println(string); //debug
-       delay(10);
-       }
+       digitalWrite(led,LOW); 
+       Serial.print("D");
+        test="";
+        command='\0';
      }
-    */
-      delay(1000);
-  }
-    
- void ledOn()
-  {
-    digitalWrite(led,HIGH);
-   // delay(10);
-  }
- void ledOff()
-  {
-    digitalWrite(led,LOW);
-   // delay(10);
+ 
+  delay(1000);
 }
+
